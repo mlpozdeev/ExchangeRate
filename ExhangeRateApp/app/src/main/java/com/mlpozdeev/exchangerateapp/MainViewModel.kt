9c64c6f9bec6b49a1f2f93dbd309e34afbaa1b13
Maksim Pozdeev <m.l.pozdeev@gmail.com>
2022-03-19 00:07:49 +0400

    main viewmodel created

---
+++ ExhangeRateApp/app/src/main/java/com/mlpozdeev/exchangerateapp/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.mlpozdeev.exchangerateapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun popularItemClicked() {
+        //todo navigate to popular screen
+    }
+
+    fun favoritesItemClicked() {
+        //todo navigate to favorites screen
+    }
+
+}
